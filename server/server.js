943377ad4f78d8a680fd19af130fc223b243947d
Vaibhavi K Kamath <vaibhavikamath@Vaibhavis-MacBook-Air.local>
2026-02-23 21:14:32 +0530

    cors changes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB Atlas'))
